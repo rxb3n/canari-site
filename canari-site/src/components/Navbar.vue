947d67dd52022181880e349a81d4f37a5c072f96
rxb3n <61215571+rxb3n@users.noreply.github.com>
2023-07-28 17:12:18 +0200

    responsive

--- canari-site/src/components/Navbar.vue
+++ canari-site/src/components/Navbar.vue
@@ -4,7 +4,7 @@
             <div class="nav-logo">
                 <a href="/"><h2 class="nav logo">CANARI</h2></a>
             </div>
-            <div class="nav-elements">
+            <div class="nav-elements" id="nav-links">
                 <a href="#hero-section"><p class="nav">Accueil</p></a>
                <a href="#about-section"><p class="nav">A propos</p></a>
 
@@ -18,8 +18,16 @@
                 <a href="#cta-section"><p class="nav">Contact</p></a>
 
             </div>
-            <button class="hamburger-menu" id="toggle-menu"><img src="../assets/menu-icon.png" width="40" height="40"></button>
+            <button @click =openMenu class="hamburger-menu" id="toggle-menu"><img src="../assets/menu-icon.png" width="40" height="40"></button>
         </div>
+
+        <div class="nav-mobile">
+          <p class="mobile-element">Accueil</p>
+          <p class="mobile-element">A propos</p>
+          <p class="mobile-element">Menu</p>
+          <p class="mobile-element">Contact</p>
+        </div>
+
     </section>
 </template>
 
@@ -28,7 +36,8 @@ export default {
     name: 'Navbar',
   data() {
     return {
-      isScrolled: false
+      isScrolled: false,
+      isActive: false
     }
   },
   created() {
@@ -40,6 +49,21 @@ export default {
   methods: {
     onScroll(event) {
       this.isScrolled = window.scrollY > 0;
+    },
+
+    openMenu(event) {
+      const toggleButton = document.getElementsByClassName('hamburger-menu')[0];
+      const navLinks = document.getElementsByClassName('nav-mobile')[0];
+
+        if (this.isActive == false) {
+          navLinks.style.transform = 'translate(-100%, 0)';
+          this.isActive = true;
+          console.log(this.isActive)
+        } else if (this.isActive == true) {
+          navLinks.style.transform = 'translate(100%, 0)';
+          this.isActive = false;
+          console.log(this.isActive)
+        }
     }
   }
 }
